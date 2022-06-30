7f2cec632bda72a16267007bd92b430ca95e36f0
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-06-30 21:18:05 +0530

    after 001 model forms video.

--- projects/views.py
+++ projects/views.py
@@ -1,20 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Project
-
-projectsList = [
-    {
-        'id': '1',
-        'title': 'Ecommerce Website',
-        'description': 'Fully functional ecommerce website',
-    },
-    {'id': '2',
-        'title': 'Portfolio Website',
-        'description': 'fully functional portfolio website', },
-    {'id': '3',
-     'title': 'Social Network Website',
-     'description': 'Social network website project', },
-]
+from .forms import ProjectForm
 
 
 def projects(request):
@@ -26,3 +13,9 @@ def projects(request):
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
     return render(request, 'projects/single-project.html', {'project': projectObj})
+
+
+def createProject(request):
+    form = ProjectForm()
+    context = {'form': form}
+    return render(request, 'projects/project-form.html', context)
