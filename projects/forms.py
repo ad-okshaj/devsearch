c51b04c2388f39eb4f7f0382fbe044af0ca21322
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-01 14:24:53 +0530

    after 002 theme installation video.

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from dataclasses import field
+from django import forms
 from django.forms import ModelForm
 from .models import Project
 
@@ -8,3 +9,18 @@ class ProjectForm(ModelForm):
         model = Project
         fields = ['title', 'featured_image', 'description',
                   'demo_link', 'source_link', 'tags']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple()
+        }
+
+    def __init__(self, *args, **kwargs):
+        super(ProjectForm, self).__init__(*args, **kwargs)
+
+        # self.fields['title'].widget.attrs.update(
+        #     {'class': 'input', 'placeholder': 'Add Title'})
+
+        # self.fields['description'].widget.attrs.update(
+        #     {'class': 'input', 'placeholder': 'Add Description'})
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
