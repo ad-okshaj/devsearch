179432bad59e0b32f4ad79e12f53a758a908e3f0
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-01 15:32:22 +0530

    after 001 users app video.

--- devsearch/urls.py
+++ devsearch/urls.py
@@ -4,9 +4,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-
     path('admin/', admin.site.urls),
-    path('', include('projects.urls')),
+    path('projects/', include('projects.urls')),
+    path('', include('users.urls')),
 ]
 
 
--- projects/models.py
+++ projects/models.py
@@ -1,9 +1,12 @@
 from email.policy import default
 from django.db import models
+from users.models import Profile
 import uuid
 
 
 class Project(models.Model):
+    owner = models.ForeignKey(
+        Profile, null=True, blank=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
     featured_image = models.ImageField(
