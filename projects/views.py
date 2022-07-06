6eea22b8bb8b759f30a7173e9c03b445224422a2
Adokshaj Bhandarkar <badokshaj57@gmail.com>
2022-07-06 11:27:38 +0530

    after 001 search video.

--- projects/views.py
+++ projects/views.py
@@ -3,13 +3,16 @@ import profile
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
-from .models import Project
+from django.db.models import Q
+
+from .models import Project, Tag
 from .forms import ProjectForm
+from .utils import searchProjects
 
 
 def projects(request):
-    projects = Project.objects.all()
-    context = {'projects': projects}
+    projects, search_query = searchProjects(request)
+    context = {'projects': projects, 'search_query': search_query}
     return render(request, 'projects/projects.html', context)
 
 
